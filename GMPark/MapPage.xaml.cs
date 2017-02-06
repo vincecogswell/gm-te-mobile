57626b655ade65ce312c54a0d20e44fa2bf13d1d
cobaurob <cobaurob@msu.edu>
2017-02-06 11:41:46 -0500

    Got maps to work on Android

--- GMPark/MapPage.xaml.cs
+++ GMPark/MapPage.xaml.cs
@@ -13,18 +13,18 @@ namespace GMPark
 			InitializeComponent();
 			//message.Text = "You are a" + role + ", and going to" + building ;
 
-			//var map = new Map(
-			//	MapSpan.FromCenterAndRadius(
-			//			new Position(37, -122), Distance.FromMiles(0.3)))
-			//{
-			//	IsShowingUser = true,
-			//	HeightRequest = 100,
-			//	WidthRequest = 960,
-			//	VerticalOptions = LayoutOptions.FillAndExpand
-			//};
-			//var stack = new StackLayout { Spacing = 0 };
-			//stack.Children.Add(map);
-			//Content = stack;
+			var map = new Map(
+				MapSpan.FromCenterAndRadius(
+						new Position(37, -122), Distance.FromMiles(0.3)))
+			{
+				IsShowingUser = true,
+				HeightRequest = 100,
+				WidthRequest = 960,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+			var stack = new StackLayout { Spacing = 0 };
+			stack.Children.Add(map);
+			Content = stack;
 		}
 	}
 }
--- GMPark/Variables.cs
+++ GMPark/Variables.cs
@@ -5,7 +5,7 @@ namespace GMPark
 	public static class Variables
 	{
 		// https://developers.google.com/maps/documentation/android-api/signup
-		public const string GOOGLE_MAPS_ANDROID_API_KEY = "AIzaSyCjPcvf091sESEaaMAKvD9RGxAoXPzkDXE";
+		public const string GOOGLE_MAPS_ANDROID_API_KEY = "AIzaSyANlIJWZ1jCib8VfrOd8OqE7509N7lKpqw";
 
 		// https://developers.google.com/maps/documentation/ios-sdk/start#step_4_get_an_api_key
 		public const string GOOGLE_MAPS_IOS_API_KEY = "AIzaSyDt5pal_PYTlmuXUpwP6cXJ-gbrb7bqYE4";

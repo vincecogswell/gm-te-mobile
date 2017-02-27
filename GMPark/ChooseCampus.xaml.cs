94e5aa22261cb7b1aacbc8f9b5dc56aa410f2fce
Zhaofeng Jin <jinzhaof@cse.msu.edu>
2017-02-27 00:50:13 -0500

    Add all data to campuses.json

---
+++ GMPark/ChooseCampus.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GMPark
+{
+	public partial class ChooseCampus : ContentPage
+	{
+		public ChooseCampus()
+		{
+			InitializeComponent();
+		}
+	}
+}

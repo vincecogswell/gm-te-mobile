8e6ce1f7667da91237b13104c5c95def03dcf9e3
Zhaofeng Jin <jinzhaof@cse.msu.edu>
2017-04-25 00:53:48 -0400

    text to speech

---
+++ GMPark/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿/* Authot : Phyllis Jin
+ * Interface for function text to speech in share code
+ */
+using System;
+namespace GMPark
+{
+	public interface ITextToSpeech
+	{
+		void Speak(string text);
+	}
+}
---
+++ iOS/TextToSpeech_iOS.cs
@@ -0,0 +1,34 @@
+﻿/* Authot : Phyllis Jin
+ * class for function text to speech that works on iOS platform
+ */
+using System;
+using AVFoundation;
+using Xamarin.Forms;
+using GMPark.iOS;
+
+[assembly: Dependency(typeof(TextToSpeech_IOS))]
+
+namespace GMPark.iOS
+{
+	public class TextToSpeech_IOS : ITextToSpeech
+	{
+		public TextToSpeech_IOS()
+		{
+		}
+		// Overwirte Speak method in share code
+		public void Speak(string text)
+		{
+			var speechSynthesizer = new AVSpeechSynthesizer();
+
+			var speechUtterance = new AVSpeechUtterance(text)
+			{
+				Rate = AVSpeechUtterance.MaximumSpeechRate / 2,
+				Voice = AVSpeechSynthesisVoice.FromLanguage("en-US"),
+				Volume = 0.5f,
+				PitchMultiplier = 1.0f
+			};
+
+			speechSynthesizer.SpeakUtterance(speechUtterance);
+		}
+	}
+}
